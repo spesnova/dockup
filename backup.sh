cbbe3cd331321e66da954dbd02609b74c58b2fb0
Seigo Uchida <spesnova@gmail.com>
2016-03-07 19:40:48 +0900

    Fix backup path

--- backup.sh
+++ backup.sh
@@ -4,11 +4,11 @@ echo "=> Backup has been started"
 
 # Get timestamp
 : ${BACKUP_SUFFIX:=.$(date +"%Y-%m-%d-%H-%M-%S")}
-readonly tarball=/$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
+readonly tarball=$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
 
 # Create a gzip compressed tarball with the volume(s)
-echo "=> Creating compressed backup: $tarball"
-tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
+echo "=> Creating compressed backup: /$tarball"
+tar czf /$tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
 
 # Create bucket, if it doesn't already exist
 aws s3 ls s3://$S3_BUCKET_NAME >/dev/null 2>&1
@@ -18,8 +18,8 @@ then
 fi
 
 # Upload the backup to S3 with timestamp
-echo "=> Uploading the backup to S3: $tarball"
-aws s3 --region $AWS_DEFAULT_REGION cp $tarball s3://$S3_BUCKET_NAME/$BACKUP_NAME/$tarball
+echo "=> Uploading the backup to S3: /$tarball"
+aws s3 --region $AWS_DEFAULT_REGION cp /$tarball s3://$S3_BUCKET_NAME/$BACKUP_NAME/$tarball
 
 # Dispose of old backups
 if [[ -n "$MAX_NUMBER_OF_BACKUPS" ]]; then
@@ -39,7 +39,7 @@ if [[ -n "$MAX_NUMBER_OF_BACKUPS" ]]; then
 fi
 
 # Dispose of local backup
-echo "=> Removing local backup: $tarball"
-rm $tarball
+echo "=> Removing local backup: /$tarball"
+rm /$tarball
 
 echo "=> Backup has been completed"
